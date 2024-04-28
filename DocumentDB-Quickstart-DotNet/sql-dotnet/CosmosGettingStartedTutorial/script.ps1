d61edbb2a36097b6ed81d119ff78845f918adcb0
Dan Radu <radudanielro@yahoo.com>
2024-04-28 10:41:21 +1000

    Minor update for cloud shell

--- DocumentDB-Quickstart-DotNet/sql-dotnet/CosmosGettingStartedTutorial/script.ps1
+++ DocumentDB-Quickstart-DotNet/sql-dotnet/CosmosGettingStartedTutorial/script.ps1
@@ -3,3 +3,6 @@ $prop.Node
 $prop.Node.SetAttribute("value", "k1k2k3")
 $prop.Node.OwnerDocument.Save(".\DocumentDB-Quickstart-DotNet\sql-dotnet\CosmosGettingStartedTutorial\App.config")
 $prop.Node
+
+$env:HOME
+$prop.Node.OwnerDocument.Save("$($env:HOME)/cosmosdb-quick-start-dotnet/DocumentDB-Quickstart-DotNet/sql-dotnet/CosmosGettingStartedTutorial/App.config")
